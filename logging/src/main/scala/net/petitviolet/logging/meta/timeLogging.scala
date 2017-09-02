a8d9fbd679178ce066ebc837923389f982fab7f5
petitviolet <violethero0820@gmail.com>
2017-09-02 21:02:17 +0900

    implemented @timeLogging

---
+++ logging/src/main/scala/net/petitviolet/logging/meta/timeLogging.scala
@@ -0,0 +1,47 @@
+package net.petitviolet.logging.meta
+
+import scala.annotation.{ StaticAnnotation, compileTimeOnly }
+import scala.meta._
+
+@compileTimeOnly("timeLogging not expanded")
+class timeLogging(outF: (String) => Unit = println) extends StaticAnnotation {
+  inline def apply(defn: Any): Any = meta {
+    defn match {
+      case d: Defn.Def =>
+        d.copy(body = timeLogging.newBody(this)(d))
+      case _ =>
+        abort("annotate only function!")
+    }
+  }
+}
+
+private object timeLogging {
+  /**
+   * create new method body with logging before and after invoking method
+   */
+  def newBody(self: Stat)(method: Defn.Def): Term = {
+    // define complicated names, because to avoid use duplicate names which are defined by programmer
+    val time = q"System.currentTimeMillis"
+    val name4log = Term.Name("timeLogging$methodNameAndParamsForLogging")
+    val pat4log = Pat.Var.Term(name4log)
+    val name4result = Term.Name("timeLogging$resultOfMethod")
+    val pat4result = Pat.Var.Term(name4result)
+
+    // names for tracking method invocation time
+    val name4Start = Term.Name("timeLogging$startTime")
+    val pat4Start = Pat.Var.Term(name4Start)
+    val name4End = Term.Name("timeLogging$endTime")
+    val pat4End = Pat.Var.Term(name4End)
+
+    // let `caller` as a lazy val, for cases of `out` will not be invoked on some environment like production
+    q"""
+     lazy val $pat4log: String = ${caller(method)}
+     ${out(self)}("[start]" + $name4log)
+     val $pat4Start = $time
+     val $pat4result = ${method.body}
+     val $pat4End = $time
+     ${out(self)}("[end][" + ($name4End - $name4Start) + " ms]" + $name4log + s" => " + $name4result)
+     $name4result
+     """
+  }
+}
---
+++ sample/src/main/scala/net/petitviolet/logging/meta/timeLoggingApp.scala
@@ -0,0 +1,40 @@
+package net.petitviolet.logging.meta
+
+import java.time.LocalDateTime
+
+object timeLoggingApp extends App {
+  val logger = new {
+    def info(s: => String): Unit =
+      println(s"${LocalDateTime.now()}: $s")
+  }
+
+  @timeLogging(logger.info)
+  def add(i: Int, j: Int): Int = {
+    i + j
+  }
+  println(add(1, 2))
+
+  @timeLogging(logger.info)
+  def add2(i: Int)(j: Int)(): Int = {
+    Thread.sleep(100)
+    i + j
+  }
+
+  println(add(1, 2))
+  println(add2(2)(3))
+  val add4: Int => () => Int = add2(4)
+  println(add4(5)())
+
+  case class User(name: String) extends AnyVal {
+    @timeLogging(println) def greet(s: String = "")(implicit i: Int): String = s"hello $name! * $i"
+  }
+
+  @timeLogging
+  def create(name: String): User = {
+    User(name)
+  }
+  implicit val i: Int = 100
+
+  println(create("Alice"))
+  println(create("Alice").greet())
+}
